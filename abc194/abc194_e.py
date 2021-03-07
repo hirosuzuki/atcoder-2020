3fdbfb3bab2e4f17c883a457ad2d2bde45b4f70c
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2021-03-07 14:18:01 +0900

    ABC 194 E (ANS)

--- abc194/abc194_e.py
+++ abc194/abc194_e.py
@@ -4,34 +4,24 @@ N, M = [int(_) for _ in input().split()]
 A = [int(_) for _ in input().split()]
 
 def solve(N: int, M: int, A: List[int]):
-    ns = [0] * 1500001
 
-    r = 0
-    result = 1500001
+    ns = [-1] * 1500001
+    ms = [0] * 1500001
+    for i in range(N):
+        x = A[i]
+        ms[x] = max(ms[x], i - ns[x])
+        ns[x] = i
+    
+    i = N
+    for x in range(1500001):
+        ms[x] = max(ms[x], i - ns[x])
 
-    def append(n: int):
-        nonlocal r
-        ns[n] += 1
-        while ns[r] > 0:
-            r += 1
+    #print(ns[:10])
+    #print(ms[:10])
 
-    def remove(n: int):
-        nonlocal r
-        ns[n] -= 1
-        if ns[n] == 0 and n < r:
-            r = n
-
-    for i in range(M):
-        append(A[i])
-    result = min(result, r)
-    #print("*", r, ns[:10])
-
-    for i in range(N - M):
-        if A[i + M] != A[i]:
-            append(A[i + M])
-            remove(A[i])
-            result = min(result, r)
-        #print("*", r, ns[:10])
+    result = 0
+    while ms[result] <= M:
+        result += 1
 
     print(result)
 
